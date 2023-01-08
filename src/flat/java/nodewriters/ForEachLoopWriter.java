5466e890ab3809262d7aec4098c4908433c8a540
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-01-08 17:56:54 -0500

    Write iterator element declaration for foreach loops

--- src/flat/java/nodewriters/ForEachLoopWriter.java
+++ src/flat/java/nodewriters/ForEachLoopWriter.java
@@ -11,15 +11,22 @@ public abstract class ForEachLoopWriter extends LoopWriter
 	{
 		Value hasNextCheck = node().getHasNextCheck();
 		
-		builder.append("while (").append(getWriter(hasNextCheck).writeExpression()).append(") ");
-		
+		builder.append("while (").append(getWriter(hasNextCheck).writeExpression()).append(") {\n");
+
+		writeIteratorElementDeclaration(builder);
+
 		node().forEachChild(child -> {
-			if (child != node().getArgumentList())
-			{
+			if (child != node().getArgumentList()) {
 				getWriter(child).write(builder);
 			}
 		});
 		
-		return builder;
+		return builder.append("}\n");
+	}
+
+	public StringBuilder writeIteratorElementDeclaration(StringBuilder builder) {
+		getWriter(node().getVariable()).writeType(builder, true);
+		getWriter(node().getVariable()).writeName(builder);
+		return builder.append(";\n");
 	}
 }
