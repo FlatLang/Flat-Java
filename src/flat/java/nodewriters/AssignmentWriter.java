8b2db4261d7863b7bbda71fa5938904e8d07e0ce
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-01-09 09:37:37 -0500

    Cast assignments that arent exactly the same type

--- src/flat/java/nodewriters/AssignmentWriter.java
+++ src/flat/java/nodewriters/AssignmentWriter.java
@@ -9,6 +9,19 @@ public abstract class AssignmentWriter extends ValueWriter
 	@Override
 	public StringBuilder writeExpression(StringBuilder builder)
 	{
-		return getWriter(node().getAssigneeNode()).writeExpression(builder).append(" = ").append(getWriter(node().getAssignmentNode()).writeExpression());
+		getWriter(node().getAssigneeNode()).writeExpression(builder).append(" = ");
+
+		Value leftType = node().getAssigneeNode().getReturnedNode();
+		Value rightType = node().getAssignmentNode().getReturnedNode();
+		String leftTypeStr = getWriter(leftType).writeType(new StringBuilder(), false).toString();
+		String rightTypeStr = getWriter(rightType).writeType(new StringBuilder(), false).toString();
+
+		if (!leftTypeStr.equals(rightTypeStr)) {
+			builder.append('(');
+			builder.append(leftTypeStr);
+			builder.append(')');
+		}
+
+		return builder.append(getWriter(node().getAssignmentNode()).writeExpression());
 	}
 }
