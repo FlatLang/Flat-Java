b4b6c846ce8b4a62b66ff78013508a87774c099e
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-01-23 12:16:52 -0500

    Cast primitive operations when necessary

--- src/flat/java/nodewriters/BinaryOperationWriter.java
+++ src/flat/java/nodewriters/BinaryOperationWriter.java
@@ -5,16 +5,42 @@ import flat.tree.*;
 public abstract class BinaryOperationWriter extends IValueWriter
 {
 	public abstract BinaryOperation node();
-	
+
 	@Override
 	public StringBuilder writeExpression(StringBuilder builder)
 	{
+		boolean cast = requiresCast();
+
+		if (cast) {
+			builder.append("(");
+			writeType(builder, false);
+			builder.append(")");
+			builder.append("(");
+		}
+
 		getWriter(node().getLeftOperand()).writeExpression(builder);
-		
-		builder.append(' ').append(getWriter(node().getOperator()).write()).append(' ');
-		
-		builder.append(getWriter(node().getRightOperand()).writeExpression());
+
+		writeOperator(builder);
+
+		getWriter(node().getRightOperand()).writeExpression(builder);
+
+		if (cast) {
+			builder.append(")");
+		}
 		
 		return builder;
+	}
+
+	public StringBuilder writeOperator(StringBuilder builder) {
+		builder.append(' ');
+		getWriter(node().getOperator()).write(builder);
+		return builder.append(' ');
+	}
+
+	private boolean requiresCast() {
+		return node().getParent() instanceof BinaryOperation == false &&
+			node().isPrimitive() &&
+			node().getOperator().isArithmetic() &&
+			!node().getOperator().isShorthand();
 	}
 }
