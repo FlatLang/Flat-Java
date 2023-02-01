7048a4b623ab668ed6a62b666cc76d318436b34e
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-01-31 22:14:46 -0500

    Fix else statement scope contents

--- src/flat/java/nodewriters/ElseStatementWriter.java
+++ src/flat/java/nodewriters/ElseStatementWriter.java
@@ -9,21 +9,22 @@ public abstract class ElseStatementWriter extends ControlStatementWriter
 	@Override
 	public StringBuilder write(StringBuilder builder)
 	{
-		builder.append("else");
-		
+		builder.append("else ");
+
 		if (node().getNumChildren() == 2)
 		{
 			Node child = node().getChild(1);
-			
+
 			if (child instanceof IfStatement)
 			{
-				builder.append(' ');
-				getWriter(child).writeExpression(builder);
+				getWriter(child).writeExpression(builder).append(' ');
+
+				getWriter(child.getScope()).write(builder, true, false);
+
+				return builder.append(node().getNextNode() instanceof ElseStatement ? ' ' : '\n');
 			}
 		}
-		
-		builder.append(' ');
-		
+
 		return getWriter(node().getScope()).write(builder);
 	}
 }
