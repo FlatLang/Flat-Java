566f2eeaafa0c8272ac73ab6823b228fa7f72044
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-02-09 20:37:42 -0500

    Fix generic type arguments with function types

--- src/flat/java/nodewriters/GenericTypeArgumentWriter.java
+++ src/flat/java/nodewriters/GenericTypeArgumentWriter.java
@@ -1,11 +1,36 @@
 package flat.java.nodewriters;
 
+import flat.tree.ClassDeclaration;
+import flat.tree.FirstClassClosureDeclaration;
+import flat.tree.FunctionType;
 import flat.tree.Value;
 import flat.tree.generics.GenericTypeArgument;
 
 public abstract class GenericTypeArgumentWriter extends IIdentifierWriter
 {
     public abstract GenericTypeArgument node();
+
+    @Override
+    public StringBuilder writeType(StringBuilder builder, boolean space, boolean convertPrimitive, boolean boxPrimitive, Value context) {
+        if (node().getTypeObject() instanceof FunctionType) {
+            FirstClassClosureDeclaration closure = ((FunctionType) node().getTypeObject()).closure;
+
+            return getWriter(closure).writeType(builder, space, convertPrimitive, boxPrimitive, context);
+        }
+
+        return super.writeType(builder, space, convertPrimitive, boxPrimitive, context);
+    }
+
+    @Override
+    public StringBuilder writeTypeName(StringBuilder builder, boolean convertPrimitive, boolean boxPrimitive, Value context) {
+        ClassDeclaration c = node().getTypeClass();
+
+        if (c == null) {
+            return builder.append(node().getName());
+        }
+
+        return super.writeTypeName(builder, convertPrimitive, boxPrimitive, context);
+    }
 
     @Override
     public StringBuilder writeExpression(StringBuilder builder) {
