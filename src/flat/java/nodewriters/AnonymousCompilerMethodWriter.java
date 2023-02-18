9c3a8c503e9cd3634e06159ffcdd5a32bf2d458c
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-02-18 14:22:53 -0500

    Update anonymous functions to always be static in extension classes

--- src/flat/java/nodewriters/AnonymousCompilerMethodWriter.java
+++ src/flat/java/nodewriters/AnonymousCompilerMethodWriter.java
@@ -1,10 +1,20 @@
 package flat.java.nodewriters;
 
 import flat.tree.AnonymousCompilerMethod;
+import flat.tree.ExtensionDeclaration;
 
 public abstract class AnonymousCompilerMethodWriter extends BodyMethodDeclarationWriter
 {
 	public abstract AnonymousCompilerMethod node();
+
+	@Override
+	public StringBuilder writeStatic(StringBuilder builder) {
+		if (node().getParentClass() instanceof ExtensionDeclaration) {
+			return builder.append("static ");
+		}
+
+		return super.writeStatic(builder);
+	}
 
 	@Override
 	public StringBuilder writeStaticMethodInstanceOverload(StringBuilder builder) {
