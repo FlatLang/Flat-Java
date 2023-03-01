7fba6c504fb7d0c3df9bf54f902ca48ba9e9def8
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-03-01 15:13:42 -0500

    Handle writing explicit generic arguments for method calls

--- src/flat/java/nodewriters/MethodCallWriter.java
+++ src/flat/java/nodewriters/MethodCallWriter.java
@@ -1,6 +1,7 @@
 package flat.java.nodewriters;
 
 import flat.tree.*;
+import flat.tree.generics.GenericTypeArgumentList;
 
 public abstract class MethodCallWriter extends VariableWriter
 {
@@ -25,6 +26,11 @@ public abstract class MethodCallWriter extends VariableWriter
 	}
 
 	@Override
+	public GenericTypeArgumentList getGenericTypeArgumentList() {
+		return node().getMethodGenericTypeArgumentList();
+	}
+
+	@Override
 	public StringBuilder writeExtensionReferenceAccess(StringBuilder builder) {
 		if (node().declaration instanceof AnonymousCompilerMethod) return builder;
 
@@ -32,16 +38,54 @@ public abstract class MethodCallWriter extends VariableWriter
 	}
 
 	@Override
+	public StringBuilder writeGenericArguments(StringBuilder builder, Value context) {
+		if (getGenericTypeArgumentList().getChildTypeStream().anyMatch(a -> !a.autoAdded)) {
+			return super.writeGenericArguments(builder, context);
+		}
+
+		return builder;
+	}
+
+	@Override
 	public StringBuilder writeUseExpression(StringBuilder builder)
 	{
 		if (isExtensionDeclaration()) {
 			return writeExtensionUseExpression(builder, node());
-		} else {
-			writeExtensionReferenceAccess(builder);
+		}
+
+		StringBuilder prefix = new StringBuilder();
+
+		writeExtensionReferenceAccess(prefix);
+
+		StringBuilder genericArgs = node().getCallableDeclaration() instanceof ClosureDeclaration
+			? new StringBuilder()
+			: writeGenericArguments(new StringBuilder());
+
+		builder.append(prefix);
+
+		if (
+			genericArgs.length() > 0 &&
+				node().declaration instanceof Constructor == false &&
+				!node().isAccessed() &&
+				prefix.length() == 0
+		) {
+			if (node().isAccessedWithinStaticContext()) {
+				getWriter((Identifier) node().getReferenceTypeNode()).writeName(builder);
+			} else {
+				builder.append("this");
+			}
+
+			builder.append('.');
+
+			builder.append(genericArgs);
 		}
 
 		writeName(builder);
-		
+
+		if (genericArgs.length() > 0 && node().declaration instanceof Constructor) {
+			builder.append(genericArgs);
+		}
+
 		if (node().getCallableDeclaration() instanceof ClosureDeclaration)
 		{
 			builder.append(".call");
